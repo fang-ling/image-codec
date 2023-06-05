fb44f4a28f6fff9bec0dbae3622563d6de0b40f3
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-06-05 10:29:07 +0800

    Add generic decode API

--- Sources/txt/txt.swift
+++ Sources/txt/txt.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
-public struct Decoder { }
+public struct Decoder {
+    @inlinable public static func decode(from file : String) -> RGBA64? {
+        let lc = file.lowercased()
+        if lc.hasSuffix(".jpeg") || lc.hasSuffix(".jpg") {
+            return decode(from_jpeg: file)
+        } else if lc.hasSuffix(".png") {
+            return decode(from_png: file)
+        } else if lc.hasSuffix(".heif") || lc.hasSuffix(".heic") {
+            return decode(from_heif: file)
+        } else if lc.hasSuffix(".webp") {
+            return decode(from_webp: file)
+        } else { /* Currently unsupported */
+            return nil
+        }
+    }
+}
 
 public struct Encoder { }
