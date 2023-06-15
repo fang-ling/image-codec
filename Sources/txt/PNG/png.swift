4a296132c2e483d73c5cb7aa781899ac05e3f369
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-06-15 13:09:10 +0800

    Improve error handling in png

--- Sources/txt/PNG/png.swift
+++ Sources/txt/PNG/png.swift
@@ -9,23 +9,28 @@ import PNG
 import Foundation
 
 extension Decoder {
-    @inlinable public static func decode(from_png png : String) -> RGBA64 {
-        guard let image : PNG.Data.Rectangular = try! .decompress(
-                path: png
-              ) else {
-            fatalError("reading png file")
+    @inlinable public static func decode(from_png png : String) -> RGBA64? {
+        do {
+            guard let image : PNG.Data.Rectangular = try .decompress(
+                    path: png
+                  ) else {
+                fatalError("reading png file")
+            }
+            let _rgba32 : [PNG.RGBA<UInt8>] = image.unpack(
+              as: PNG.RGBA<UInt8>.self
+            )
+            var rgba32 = [UInt8]()
+            for pixel in _rgba32 {
+                rgba32.append(pixel.r)
+                rgba32.append(pixel.g)
+                rgba32.append(pixel.b)
+                rgba32.append(pixel.a)
+            }
+            let size = image.size
+            return RGBA64(width: size.x, height: size.y, rgba32: rgba32)
+        } catch {
+            print("error decompress png file: \(png)")
+            return nil
         }
-        let _rgba32 : [PNG.RGBA<UInt8>] = image.unpack(
-          as: PNG.RGBA<UInt8>.self
-        )
-        var rgba32 = [UInt8]()
-        for pixel in _rgba32 {
-            rgba32.append(pixel.r)
-            rgba32.append(pixel.g)
-            rgba32.append(pixel.b)
-            rgba32.append(pixel.a)
-        }
-        let size = image.size
-        return RGBA64(width: size.x, height: size.y, rgba32: rgba32)
     }
 }
