4f5addabd68d1cf30d3cf8bd661ca8a02ac8282a
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-09-17 13:33:36 +0800

    Refact: Standardizing Arbitrary Image Formats for Processing

--- Sources/ImageCodec/ImageCodec.swift
+++ Sources/ImageCodec/ImageCodec.swift
@@ -10,6 +10,9 @@ import Foundation
 import ImageIO
 import UniformTypeIdentifiers
 
+@usableFromInline
+let BPC = 16
+
 public struct PixelBuffer {
   public var width : Int
   public var height : Int
@@ -19,8 +22,7 @@ public struct PixelBuffer {
   public var bitmap_info : CGBitmapInfo
   public var properties : CFDictionary?
 
-  public var array : [UInt8]
-  public var array_16 : [UInt16]
+  public var array : [UInt16]
 
   public init(
     width: Int,
@@ -40,11 +42,13 @@ public struct PixelBuffer {
     self.properties = properties
     
     array = []
-    array_16 = []
   }
 }
 
-/// Reads image data from a file path into RGB(A) interleaved buffer.
+/// Reads image data from a file and convert it into a four-channel,
+/// 16-bit-per-channel ARGB interleaved buffer.
+///
+/// Color space and metadata are copied.
 @inlinable
 public func image_decode(file_path: String) -> PixelBuffer? {
   /* Create CGImageSource */
@@ -64,32 +68,41 @@ public func image_decode(file_path: String) -> PixelBuffer? {
   }
   /* Create vImage_PixelBuffer */
   do {
-    let (buf, format) =
+    /* Create the Source and Destination Image Formats */
+    let (src_buf, src_format) =
       try vImage.PixelBuffer.makeDynamicPixelBufferAndCGImageFormat(
         cgImage: cg_img
       )
-    print(cg_img)
+    guard let dst_format = vImage_CGImageFormat(
+      bitsPerComponent: BPC,
+      bitsPerPixel: BPC * 4,
+      colorSpace: cg_img.colorSpace!,
+      bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.noneSkipFirst.rawValue)
+    ) else {
+      print("unable to initialize cgImageformat")
+      return nil
+    }
+    /* Create the Destination Buffers */
+    let dst_buf = vImage.PixelBuffer<vImage.Interleaved16Ux4>(size: src_buf.size)
+    /* Perform the conversion */
+    let converter = try
+      vImageConverter.make(
+        sourceFormat: src_format,
+        destinationFormat: dst_format
+      )
+    try converter.convert(from: src_buf, to: dst_buf)
+    
     var pixel_buf =
       PixelBuffer.init(
         width: cg_img.width,
         height: cg_img.height,
-        bits_per_component: cg_img.bitsPerComponent,
-        component_count: format.componentCount,
+        bits_per_component: Int(dst_format.bitsPerComponent),
+        component_count: dst_format.componentCount,
         color_space: cg_img.colorSpace!,
-        bitmap_info: format.bitmapInfo,
+        bitmap_info: dst_format.bitmapInfo,
         properties: properties
       )
-    if cg_img.bitsPerComponent <= 8 {
-      pixel_buf.array = buf.makeArray(
-        of: UInt8.self,
-        channelCount: format.componentCount
-      )
-    } else {
-      pixel_buf.array_16 = buf.makeArray(
-        of: UInt16.self,
-        channelCount: format.componentCount
-      )
-    }
+    pixel_buf.array = dst_buf.array
     return pixel_buf
   } catch {
     print(error)
@@ -121,23 +134,12 @@ public func image_encode(
     return
   }
   /* Create CGImage */
-  var cg_img : CGImage?
-  if pixel_buffer.bits_per_component <= 8 { /* Create vImage_PixelBuffer */
-    let buf = vImage.PixelBuffer<vImage.Interleaved8x4>(
-      data: &pixel_buffer.array,
-      width: pixel_buffer.width,
-      height: pixel_buffer.height
-    )
-    cg_img = buf.makeCGImage(cgImageFormat: format)
-  } else {
-    let buf = vImage.PixelBuffer<vImage.Interleaved16Ux4>(
-      data: &pixel_buffer.array_16,
-      width: pixel_buffer.width,
-      height: pixel_buffer.height
-    )
-    cg_img = buf.makeCGImage(cgImageFormat: format)
-  }
-  guard let cg_img else {
+  let buf = vImage.PixelBuffer<vImage.Interleaved16Ux4>(
+    data: &pixel_buffer.array,
+    width: pixel_buffer.width,
+    height: pixel_buffer.height
+  )
+  guard let cg_img = buf.makeCGImage(cgImageFormat: format) else {
     print("unable to make CGImage from vImage_Buffer")
     return
   }
